34c4b279c25b30327e025a60145b247a8d5180ab
cheeyoong0604 <wongcy-wa15@student.tarc.edu.my>
2018-11-01 17:07:25 +0800

    update ui

---
+++ app/src/main/java/my/edu/taruc/lab2_2menu/AboutUsActivity.java
@@ -0,0 +1,52 @@
+package my.edu.taruc.lab2_2menu;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us2);
+    }
+
+    public void visitTARUC(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.tarc.edu.my"));
+        if(intent.resolveActivity(getPackageManager())!= null){
+            startActivity(intent);
+        }else {
+            Toast.makeText(this,"No app to handle this",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void showmMain(View view){
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        startActivity(intent);
+    }
+
+    public void showSendTo(View view){
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto" + "info@tarc.edu.my"));
+        if(intent.resolveActivity(getPackageManager())!= null){
+            startActivity(intent);
+        }else {
+            Toast.makeText(this,"No app to handle this",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void showDial(View view){
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("tel" + "034123456"));
+        if(intent.resolveActivity(getPackageManager())!= null){
+            startActivity(intent);
+        }else {
+            Toast.makeText(this,"No app to handle this",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
